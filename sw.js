92be53320f6241340c4125ee0d91fdffffde4a93
Menghua <zoeaeen13@gmail.com>
2020-12-21 21:35:39 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ffe13d860cf527551655.js"
+    "url": "webpack-runtime-000ac37f328683e72cc8.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5e26f41d0a058c965c67b700759be13b"
+    "revision": "9b378c3f1c08749994f1635b59c0d482"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "89a57a490ad043adb7c3aeed8547c4e4"
+    "revision": "f0d5b32cd27561068bbe40303f618630"
   },
   {
     "url": "polyfill-ca8adf9770cfeec4c092.js"
